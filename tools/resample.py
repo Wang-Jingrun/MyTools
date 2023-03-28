a49c8d246ea707520c481db03acf80da7e19e44c
Wang-Jingrun <108912810+Wang-Jingrun@users.noreply.github.com>
2023-03-28 19:02:41 +0800

    Update resample.py

--- tools/resample.py
+++ tools/resample.py
@@ -5,16 +5,15 @@ import soundfile
 if __name__ == '__main__':
     fs = 16000
 
-    origin_path = './SCAFE/'
+    origin_path = './test_noisy_speech'
     files = os.listdir(origin_path)
 
-
-    resample_path = f'./SCAFE-rs/'
+    resample_path = f'./test_noisy_speech-rs'
     if not os.path.exists(resample_path):  # 创建保存的目录
         os.mkdir(resample_path)
 
     for file in files:
         if file[-4:] == '.wav':
             print(file)
-            sample, _ = librosa.load(origin_path + file, sr=fs)
-            soundfile.write(resample_path + file[:-4] + '.wav', sample, fs)
+            sample, _ = librosa.load(os.path.join(origin_path, file), sr=fs)
+            soundfile.write(os.path.join(resample_path, file[:-4] + '.wav'), sample, fs)
